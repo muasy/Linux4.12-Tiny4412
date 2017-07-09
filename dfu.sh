d593bff1fb9f13f17a905987f86fade1b1211484
SY <1530454315@qq.com>
2017-07-09 19:02:33 +0800

    2017-7-9 19:02:11【增加 Initamfs文件系统】

---
+++ dfu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Developer: SY
+# Data     : 2017-7-8 12:21:09
+# Function : DFU 
+
+echo "------------------------------"
+echo "DFU List"
+dfu-util -l
+
+echo "------------------------------"
+echo "DFU uImage"
+dfu-util -D arch/arm/boot/uImage -a 0
+
+echo "------------------------------"
+echo "DFU exynos4412-tiny4412.dtb"
+dfu-util -D arch/arm/boot/dts/exynos4412-tiny4412.dtb -a 1
+
+echo "------------------------------"
+echo "over"
+
+
--- make_auto.sh
+++ make_auto.sh
@@ -8,6 +8,9 @@ echo "------------------------------"
 echo "make"
 make ARCH=arm uImage LOADADDR=0X40008000 -j4
 
+echo "make dts"
+make ARCH=arm dtbs
+
 echo "backup config"
 cp .config tiny4412_defconfig
 
