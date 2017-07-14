e5500462c098a4364bded733c6d6e1d327a1acea
SY <1530454315@qq.com>
2017-07-14 22:09:08 +0800

    2017-7-14 22:08:55【移植eMMC驱动完成】

--- make_auto.sh
+++ make_auto.sh
@@ -8,11 +8,17 @@ echo "------------------------------"
 echo "make"
 make ARCH=arm uImage LOADADDR=0X40008000 -j4
 
+echo "------------------------------"
 echo "make dts"
 make ARCH=arm dtbs
 
+echo "------------------------------"
 echo "backup config"
 cp .config tiny4412_defconfig
+
+echo "------------------------------"
+echo "cp file to tftpboot"
+./tftp.sh
 
 echo "over"
 
